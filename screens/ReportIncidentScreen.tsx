31888fdf276773ac3162599c16b421c7e65e79a7
Mostafa Ali <=>
2024-11-09 20:53:50 -0600

    Stash

--- screens/ReportIncidentScreen.tsx
+++ screens/ReportIncidentScreen.tsx
@@ -1,4 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
+import * as Location from 'expo-location';
+import { useEffect, useState } from "react";
+
 
 const Styles = StyleSheet.create({
     container: {
@@ -8,7 +11,27 @@ const Styles = StyleSheet.create({
     },
 });
 
-export default function ReportIncidentScreen() {    
+export default function ReportIncidentScreen() {
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+    useEffect(() => {
+        (async () => {
+          
+          let { status } = await Location.requestForegroundPermissionsAsync();
+          if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+          }
+    
+          let location = await Location.getCurrentPositionAsync({});
+          setLocation(location);
+        })();
+      }, []);
+
+      console.log(location);
+      
+
     return (
         <View style={Styles.container}>
             <Text>Report</Text>
